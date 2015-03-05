55ea8c77ceca88af9a509a1fa0808d424ddf07e7
xamgun <xamgun@gmail.com>
2015-03-05 13:02:24 +0000

    add_model_product

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   
-  root 'users#index'
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
