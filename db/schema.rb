d2b7aff3ef92bcd4f73cc69f6113d56f4f287bd7
xamgun <xamgun@gmail.com>
2015-04-23 20:37:57 +0000

    added_email_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423130907) do
+ActiveRecord::Schema.define(version: 20150423203413) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150423130907) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "email"
   end
 
 end
