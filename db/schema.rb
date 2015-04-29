7771c3a67e7350400698213b0c2851b2e054e7e2
xamgun <xamgun@gmail.com>
2015-04-29 01:25:38 +0000

    merge_to_master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423203413) do
+ActiveRecord::Schema.define(version: 20150427175011) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150423203413) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "email"
+    t.datetime "confirmed_at"
+    t.string   "confirmation_token"
   end
 
 end
